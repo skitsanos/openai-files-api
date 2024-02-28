fb211d6a9dac6d3b0f0238dbae6ede67645a1ab3
skitsanos <info@skitsanos.com>
2024-02-28 14:06:46 +0200

    chore: added error handling for file listing endpoint

--- routes/api/files/get.py
+++ routes/api/files/get.py
@@ -1,7 +1,15 @@
 from openai import OpenAI
+from flask import current_app, make_response
 
 client = OpenAI()
 
 
 def handler():
-    return client.files.list().dict()
+    try:
+        return client.files.list().dict()
+    except Exception as e:
+        current_app.logger.error(f'{e.body["message"]}')
+
+        return make_response({
+            "error": e.body["message"]
+        }, 400)
